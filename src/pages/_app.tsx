0060483fe3f8913eb196d28d420adaa2dc77323d
HoctoPrism <nemexxix@gmail.com>
2023-08-26 18:41:29 +0200

    preset MUI theme

---
+++ src/pages/_app.tsx
@@ -0,0 +1,34 @@
+// pages/_app.tsx
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import defaultTheme from '../utils/theme/defaultTheme';
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../utils/createEmotionCache";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+// later we'll modify this to its own file
+const theme = createTheme();
+export interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+    // If there's no emotionCache rendered by the server, use the clientSideEmotionCache
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={defaultTheme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+        );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+// pages/index.tsx
+import type { NextPage } from "next";
+
+import { Typography } from "@mui/material";
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Typography variant="h2" textAlign="center">
+                Hello, World
+            </Typography>
+        </div>
+        );
+};
+
+export default Home;
