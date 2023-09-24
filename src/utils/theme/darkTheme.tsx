7180b65241d156f39a37453669883319c0a1c08d
HoctoPrism <nemexxix@gmail.com>
2023-09-24 22:32:50 +0200

    update theme to support custom definitions of breakpoints

--- src/utils/theme/darkTheme.tsx
+++ src/utils/theme/darkTheme.tsx
@@ -95,6 +95,15 @@ export const darkTheme = {
     fontWeightBold: 700,
     htmlFontSize: 16,
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
   components: {
     MuiInput: {
       styleOverrides: {
--- src/utils/theme/lightTheme.tsx
+++ src/utils/theme/lightTheme.tsx
@@ -95,6 +95,15 @@ export const lightTheme = {
     fontWeightBold: 700,
     htmlFontSize: 16,
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
   components: {
     MuiButton: {
       styleOverrides: {
