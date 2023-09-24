2425e397312ac1e8724523f05ec3095ad8240920
HoctoPrism <nemexxix@gmail.com>
2023-09-24 22:55:01 +0200

    update theme to support custom definitions of transitions timings

--- src/utils/theme/lightTheme.tsx
+++ src/utils/theme/lightTheme.tsx
@@ -104,6 +104,17 @@ export const lightTheme = {
       xl: 1536,
     },
   },
+  transitions: {
+    duration: {
+      shortest: 150,
+      shorter: 200,
+      short: 250, // most basic recommended timing
+      standard: 300, // this is to be used in complex animations
+      complex: 375, // recommended when something is entering screen
+      enteringScreen: 225, // recommended when something is leaving screen
+      leavingScreen: 195,
+    },
+  },
   components: {
     MuiButton: {
       styleOverrides: {
